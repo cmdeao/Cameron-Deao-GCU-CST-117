876c8aa0e9a37b221e99da8245c7b6edb9dc4330
cmdeao <47462906+cmdeao@users.noreply.github.com>
2019-05-03 21:26:23 -0500

    Add files via upload
    A windows form application that calculates the approximation of pi based on the number of terms input by the user.

---
+++ Exercise_5_Cameron_Deao/Exercise_5_Cameron_Deao/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Exercise_5_Cameron_Deao
+{
+    partial class Pi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pi));
+            this.termsInput = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.resultsLabel = new System.Windows.Forms.Label();
+            this.instructionsLabel = new System.Windows.Forms.Label();
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // termsInput
+            // 
+            this.termsInput.Location = new System.Drawing.Point(170, 33);
+            this.termsInput.Name = "termsInput";
+            this.termsInput.Size = new System.Drawing.Size(100, 20);
+            this.termsInput.TabIndex = 0;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(39, 65);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(93, 60);
+            this.calculateButton.TabIndex = 1;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // resultsLabel
+            // 
+            this.resultsLabel.AutoSize = true;
+            this.resultsLabel.Location = new System.Drawing.Point(39, 184);
+            this.resultsLabel.Name = "resultsLabel";
+            this.resultsLabel.Size = new System.Drawing.Size(0, 13);
+            this.resultsLabel.TabIndex = 2;
+            // 
+            // instructionsLabel
+            // 
+            this.instructionsLabel.Location = new System.Drawing.Point(39, 36);
+            this.instructionsLabel.Name = "instructionsLabel";
+            this.instructionsLabel.Size = new System.Drawing.Size(100, 26);
+            this.instructionsLabel.TabIndex = 3;
+            this.instructionsLabel.Text = "Enter # of terms:";
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.AutoSize = true;
+            this.messageLabel.Location = new System.Drawing.Point(39, 157);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(0, 13);
+            this.messageLabel.TabIndex = 4;
+            // 
+            // Pi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 268);
+            this.Controls.Add(this.messageLabel);
+            this.Controls.Add(this.instructionsLabel);
+            this.Controls.Add(this.resultsLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.termsInput);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Pi";
+            this.Text = "Approximate PI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox termsInput;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label resultsLabel;
+        private System.Windows.Forms.Label instructionsLabel;
+        private System.Windows.Forms.Label messageLabel;
+    }
+}
+
---
+++ Exercise_5_Cameron_Deao/Exercise_5_Cameron_Deao/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise_5_Cameron_Deao
+{
+    public partial class Pi : Form
+    {
+        //Global variables established to be used in the program.
+        double overFour;
+        int inputTerms;
+
+        public Pi()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            //Setting all fields to correct values for the users to avoid 
+            //adding onto an existing approximation.
+            overFour = 0;
+            double sign = 1;
+            messageLabel.Text = "";
+            resultsLabel.Text = "";
+            //Exception handling to avoid incorrect data from being entered.
+            try
+            {
+                inputTerms = int.Parse(termsInput.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Please enter numeric values only.");
+            }
+            //The for loop creates an initialization expression and iterates through the loop
+            //based on the test expression. After a single loop the update expression is 
+            //called and increments the initialization expression up a single count.
+            //When the initialization expression reaches the test expression total the 
+            //for loop concludes.
+            for (int term = 0; term < inputTerms; term++)
+            {
+                //Mathematical expression that occurs for each term that is passed 
+                //into the for loop.
+                overFour += sign / (term * 2 + 1);
+                sign *= -1;
+            }
+            //Calculates the final value that will be displayed to the user.
+            double pi = 4 * overFour;
+            messageLabel.Text = "Approximate value of pi after " + inputTerms + " terms";
+            resultsLabel.Text = "= " + pi.ToString();
+        }
+    }
+}
