ce0ab15026acc48d060ea261e88d993f1deca10a
cmdeao <47462906+cmdeao@users.noreply.github.com>
2019-05-04 22:52:20 -0500

    Add files via upload
    Created a Windows Form Application that will allow the user to select a file to input into the program. Statistics are displayed to showcase the file converted to all lower-case, the first and last words alphabetically, the longest word, and the word with the most vowels. All of the statistics are displayed in text boxes within the program and output into a text file.

---
+++ Programming_Project_3_Cameron_Deao/Programming_Project_3_Cameron_Deao/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Programming_Project_3_Cameron_Deao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.fileContentsLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.lowerCaseBox = new System.Windows.Forms.TextBox();
+            this.longestWordBox = new System.Windows.Forms.TextBox();
+            this.longestWord = new System.Windows.Forms.Label();
+            this.alphabeticalLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.vowelBox = new System.Windows.Forms.TextBox();
+            this.firstAndLastBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 325);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fileContentsLabel
+            // 
+            this.fileContentsLabel.AutoSize = true;
+            this.fileContentsLabel.Location = new System.Drawing.Point(71, 47);
+            this.fileContentsLabel.Name = "fileContentsLabel";
+            this.fileContentsLabel.Size = new System.Drawing.Size(80, 13);
+            this.fileContentsLabel.TabIndex = 5;
+            this.fileContentsLabel.Text = "Contents of File";
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(119, 325);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 10;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // lowerCaseBox
+            // 
+            this.lowerCaseBox.Location = new System.Drawing.Point(38, 63);
+            this.lowerCaseBox.Multiline = true;
+            this.lowerCaseBox.Name = "lowerCaseBox";
+            this.lowerCaseBox.Size = new System.Drawing.Size(156, 256);
+            this.lowerCaseBox.TabIndex = 14;
+            // 
+            // longestWordBox
+            // 
+            this.longestWordBox.Location = new System.Drawing.Point(200, 177);
+            this.longestWordBox.Name = "longestWordBox";
+            this.longestWordBox.Size = new System.Drawing.Size(273, 20);
+            this.longestWordBox.TabIndex = 12;
+            // 
+            // longestWord
+            // 
+            this.longestWord.AutoSize = true;
+            this.longestWord.Location = new System.Drawing.Point(197, 160);
+            this.longestWord.Name = "longestWord";
+            this.longestWord.Size = new System.Drawing.Size(74, 13);
+            this.longestWord.TabIndex = 4;
+            this.longestWord.Text = "Longest Word";
+            // 
+            // alphabeticalLabel
+            // 
+            this.alphabeticalLabel.AutoSize = true;
+            this.alphabeticalLabel.Location = new System.Drawing.Point(197, 83);
+            this.alphabeticalLabel.Name = "alphabeticalLabel";
+            this.alphabeticalLabel.Size = new System.Drawing.Size(167, 13);
+            this.alphabeticalLabel.TabIndex = 6;
+            this.alphabeticalLabel.Text = "First and Last Word Alphabetically";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(200, 244);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Word With The  Most Vowels";
+            // 
+            // vowelBox
+            // 
+            this.vowelBox.Location = new System.Drawing.Point(200, 260);
+            this.vowelBox.Name = "vowelBox";
+            this.vowelBox.Size = new System.Drawing.Size(273, 20);
+            this.vowelBox.TabIndex = 11;
+            // 
+            // firstAndLastBox
+            // 
+            this.firstAndLastBox.Location = new System.Drawing.Point(200, 113);
+            this.firstAndLastBox.Name = "firstAndLastBox";
+            this.firstAndLastBox.Size = new System.Drawing.Size(273, 20);
+            this.firstAndLastBox.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 450);
+            this.Controls.Add(this.lowerCaseBox);
+            this.Controls.Add(this.firstAndLastBox);
+            this.Controls.Add(this.longestWordBox);
+            this.Controls.Add(this.vowelBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.alphabeticalLabel);
+            this.Controls.Add(this.fileContentsLabel);
+            this.Controls.Add(this.longestWord);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label fileContentsLabel;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.TextBox lowerCaseBox;
+        private System.Windows.Forms.TextBox longestWordBox;
+        private System.Windows.Forms.Label longestWord;
+        private System.Windows.Forms.Label alphabeticalLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox vowelBox;
+        private System.Windows.Forms.TextBox firstAndLastBox;
+    }
+}
+
---
+++ Programming_Project_3_Cameron_Deao/Programming_Project_3_Cameron_Deao/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Programming_Project_3_Cameron_Deao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Using the OpenFileDialog to allow the user to browse for a file.
+            OpenFileDialog dlg = new OpenFileDialog();
+            if(dlg.ShowDialog() == DialogResult.OK)
+            {
+                //Reads the contents of the file.
+                StreamReader reader = new StreamReader(dlg.FileName, Encoding.Default);
+                string readingString = reader.ReadToEnd();
+                //Converting everything read into lowercase.
+                string lowerString = readingString.ToLower();
+                lowerCaseBox.Text = lowerString.ToString();
+
+                //Creating an array to split each string within the file.
+                string[] words = readingString.Split(new[] { " " }, StringSplitOptions.None);
+                //Variables are created that will be used to complete the tasks for the assignment.
+                string word = null;
+                string vowelWord = null;
+                string firstWord = null;
+                string lastWord = null;
+                int maxVowels = 0;
+                int counter = 0;
+                //Iterates through the array to find the longest word and sets a string variable to
+                //the longest word found based on the counter.
+                foreach(String str in words)
+                {
+                    if (str.Length > counter)
+                    {
+                        word = str;
+                        counter = str.Length;
+                    }
+                }
+                //Iterates through the array to find the word with the most vowels.
+                foreach(String s in words)
+                {
+                    int vowelCounter = 0;
+                    word = word.ToLower();
+                    //A for loop checks each index of a word against all the vowels. If one is found 
+                    //the counter iterates up.
+                    for(int i = 0; i < word.Length; i++)
+                    {
+                        if(word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u')
+                        {
+                            vowelCounter++;
+                        }
+                    }
+                    //When a new max vowel string has been found the max int variable is set to the correct
+                    //amount and a string variable is set to the word that contained the most vowels.
+                    if(vowelCounter > maxVowels)
+                    {
+                        maxVowels = vowelCounter;
+                        vowelWord = word;
+                    }
+                }
+                //Sorts the entire string array into alphabetical order for use.
+                var sortedWords = from w in words orderby w select w;
+                //With each string being stored at an individual index of the array the two strings variables are assigned
+                //to the first and last index of the array.
+                firstWord = sortedWords.First();
+                lastWord = sortedWords.Last();
+                //Outputting statistics into the windows form application.
+                firstAndLastBox.Text = firstWord.ToString() + " " + lastWord.ToString();
+                vowelBox.Text = vowelWord.ToString() + " Total Vowels: " + maxVowels.ToString();
+                longestWordBox.Text = vowelWord.ToString();
+                //Closing the reader.
+                reader.Close();
+            }
+            //Created a StreamWriter to output the statistics into a file.
+            StreamWriter outputFile;
+            //Creating the text file to store the data.
+            outputFile = File.CreateText("Statistics.txt");
+            //Outputting each statistic into the file.
+            outputFile.WriteLine("Words in lowercase: " + lowerCaseBox.Text);
+            outputFile.WriteLine("First and last word alphabetically: " + firstAndLastBox.Text);
+            outputFile.WriteLine("Longest word: " + longestWordBox.Text);
+            outputFile.WriteLine("Word with most vowels: " + vowelBox.Text);
+            //Closing the output file.
+            outputFile.Close();
+            //Displosing of the diaglog.
+            dlg.Dispose();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
