6ce15aa7de259d956d14fb40b6324452ab70bc1d
cmdeao <47462906+cmdeao@users.noreply.github.com>
2019-05-08 13:16:19 -0500

    Add files via upload
    Created a windows form application that allows the user to roll a set of dice. The program will continuously roll the dice until double 1's is achieved. The number of sides on the dice is randomly set by the program to be between 4 and 20 sides. Each dice roll is output into the console and labels within the form are updated to showcase the number of sides, the number of rolls, and the final results.

---
+++ Exercise_6_Cameron_Deao/Exercise_6_Cameron_Deao/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Exercise_6_Cameron_Deao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rollButton = new System.Windows.Forms.Button();
+            this.diceLabel1 = new System.Windows.Forms.Label();
+            this.counterLabel = new System.Windows.Forms.Label();
+            this.sidesLabel = new System.Windows.Forms.Label();
+            this.diceLabel2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rollButton
+            // 
+            this.rollButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rollButton.Location = new System.Drawing.Point(12, 102);
+            this.rollButton.Name = "rollButton";
+            this.rollButton.Size = new System.Drawing.Size(138, 99);
+            this.rollButton.TabIndex = 0;
+            this.rollButton.Text = "Roll Dice";
+            this.rollButton.UseVisualStyleBackColor = true;
+            this.rollButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // diceLabel1
+            // 
+            this.diceLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.diceLabel1.Location = new System.Drawing.Point(176, 104);
+            this.diceLabel1.Name = "diceLabel1";
+            this.diceLabel1.Size = new System.Drawing.Size(80, 97);
+            this.diceLabel1.TabIndex = 2;
+            this.diceLabel1.Text = "0";
+            // 
+            // counterLabel
+            // 
+            this.counterLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.counterLabel.Location = new System.Drawing.Point(178, 259);
+            this.counterLabel.Name = "counterLabel";
+            this.counterLabel.Size = new System.Drawing.Size(178, 42);
+            this.counterLabel.TabIndex = 3;
+            // 
+            // sidesLabel
+            // 
+            this.sidesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sidesLabel.Location = new System.Drawing.Point(178, 202);
+            this.sidesLabel.Name = "sidesLabel";
+            this.sidesLabel.Size = new System.Drawing.Size(178, 57);
+            this.sidesLabel.TabIndex = 4;
+            // 
+            // diceLabel2
+            // 
+            this.diceLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.diceLabel2.Location = new System.Drawing.Point(277, 104);
+            this.diceLabel2.Name = "diceLabel2";
+            this.diceLabel2.Size = new System.Drawing.Size(80, 97);
+            this.diceLabel2.TabIndex = 5;
+            this.diceLabel2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.ClientSize = new System.Drawing.Size(470, 358);
+            this.Controls.Add(this.diceLabel2);
+            this.Controls.Add(this.sidesLabel);
+            this.Controls.Add(this.counterLabel);
+            this.Controls.Add(this.diceLabel1);
+            this.Controls.Add(this.rollButton);
+            this.Name = "Form1";
+            this.Text = "Dice Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button rollButton;
+        private System.Windows.Forms.Label diceLabel1;
+        private System.Windows.Forms.Label counterLabel;
+        private System.Windows.Forms.Label sidesLabel;
+        private System.Windows.Forms.Label diceLabel2;
+    }
+}
+
---
+++ Exercise_6_Cameron_Deao/Exercise_6_Cameron_Deao/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise_6_Cameron_Deao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+         
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Establishing an instance of the dice class.
+            Dice dice = new Dice();
+            //Creating variables to determine the number of sides
+            //on the dice and the number of rolls counter.
+            int sides = 0;
+            int counter = 0;
+            //Creating the randomizer to determine the number of 
+            //sides on the dice.
+            Random setSides = new Random();
+            //Setting the number of sides of the dice randomly between
+            //4 and 20.
+            sides = setSides.Next(4, 21);
+            dice.setSides(sides);
+            //Updating the label to showcase the number of sides
+            //on the dice.
+            sidesLabel.Text = "Number of sides on the dice: " + sides;
+            //Creating the variables used to display the results.
+            int firstDiceResult;
+            int secondDiceResult;
+            //The while loop iterates until the condition of both results
+            //equals 1.
+            while (true)
+            {
+                //Calling the function within dice to roll both dice.
+                firstDiceResult = dice.rollDie();
+                secondDiceResult = dice.rollDie2();
+                //Incrementing the counter up to count the number of rolls.
+                counter++;
+                //Showcasing each roll of the dice within the console.
+                Console.WriteLine(firstDiceResult + " " + secondDiceResult);
+                //Updating the labels to display current rolls.
+                diceLabel1.Text = firstDiceResult.ToString();
+                diceLabel2.Text = secondDiceResult.ToString();
+                diceLabel1.Update();
+                diceLabel2.Update();
+                //The while loop break condition to stop the rolls when
+                //double 1's is achieved.
+                if(firstDiceResult == 1 && secondDiceResult == 1)
+                {
+                    break;
+                }
+            }
+            //Updating dice label to the final results.
+            diceLabel1.Text = firstDiceResult.ToString();
+            diceLabel2.Text = secondDiceResult.ToString();
+            //Updating the counter label to the final results.
+            counterLabel.Text = "It took " + counter.ToString() + " rolls to get snake eyes!";
+
+            //Created the message box capability of displaying
+            //the number of rolls taken to achieve snake eyes.
+            //Left the code commented out to showcase that both will work in the program.
+            //MessageBox.Show("It took " + counter.ToString() + " rolls to get snake eyes!");
+        }
+    }
+
+    class Dice
+    {
+        //Randomizer used for the dice rolls.
+        Random randomRoll = new Random();
+        //Private int used to establish the number
+        //of sides on the dice.
+        private int sidesOfDice;
+        //Seperate int results for dice 1 and dice 2.
+        public int firstDiceResults = 0;
+        public int secondDiceResults = 0;
+        //Function used to set the number of sides on the dice.
+        public void setSides(int sides)
+        {
+            sidesOfDice = sides;
+        }
+        //Function used to roll the first dice.
+        public int rollDie()
+        {  
+            //Rolling the dice and returning the results.
+            firstDiceResults = randomRoll.Next(1, sidesOfDice + 1);
+            return firstDiceResults;
+        }
+        //Function used to roll the second dice.
+        public int rollDie2()
+        {
+            //Rolling the dice and returning the results.
+            secondDiceResults = randomRoll.Next(1, sidesOfDice - 1);
+            return secondDiceResults;
+        }
+    }
+}
