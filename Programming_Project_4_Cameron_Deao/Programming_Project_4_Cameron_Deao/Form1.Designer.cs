cd13d7d38cdc0aadc5a978180343fcfca6186d21
cmdeao <47462906+cmdeao@users.noreply.github.com>
2019-05-26 16:45:41 -0500

    Add files via upload
    Created an application that simulates two computers playing a game of tic-tac-toe.

---
+++ Programming_Project_4_Cameron_Deao/Programming_Project_4_Cameron_Deao/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Programming_Project_4_Cameron_Deao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.piece1 = new System.Windows.Forms.Label();
+            this.piece2 = new System.Windows.Forms.Label();
+            this.piece3 = new System.Windows.Forms.Label();
+            this.piece4 = new System.Windows.Forms.Label();
+            this.piece5 = new System.Windows.Forms.Label();
+            this.piece6 = new System.Windows.Forms.Label();
+            this.piece7 = new System.Windows.Forms.Label();
+            this.piece8 = new System.Windows.Forms.Label();
+            this.piece9 = new System.Windows.Forms.Label();
+            this.conditionTextBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(103, 297);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "New Game";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // piece1
+            // 
+            this.piece1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.piece1.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.piece1.Location = new System.Drawing.Point(103, 56);
+            this.piece1.Name = "piece1";
+            this.piece1.Size = new System.Drawing.Size(50, 60);
+            this.piece1.TabIndex = 1;
+            // 
+            // piece2
+            // 
+            this.piece2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.piece2.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.piece2.Location = new System.Drawing.Point(159, 56);
+            this.piece2.Name = "piece2";
+            this.piece2.Size = new System.Drawing.Size(50, 60);
+            this.piece2.TabIndex = 2;
+            // 
+            // piece3
+            // 
+            this.piece3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.piece3.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.piece3.Location = new System.Drawing.Point(215, 56);
+            this.piece3.Name = "piece3";
+            this.piece3.Size = new System.Drawing.Size(50, 60);
+            this.piece3.TabIndex = 3;
+            // 
+            // piece4
+            // 
+            this.piece4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.piece4.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.piece4.Location = new System.Drawing.Point(103, 126);
+            this.piece4.Name = "piece4";
+            this.piece4.Size = new System.Drawing.Size(50, 60);
+            this.piece4.TabIndex = 4;
+            // 
+            // piece5
+            // 
+            this.piece5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.piece5.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.piece5.Location = new System.Drawing.Point(159, 126);
+            this.piece5.Name = "piece5";
+            this.piece5.Size = new System.Drawing.Size(50, 60);
+            this.piece5.TabIndex = 5;
+            // 
+            // piece6
+            // 
+            this.piece6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.piece6.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.piece6.Location = new System.Drawing.Point(215, 126);
+            this.piece6.Name = "piece6";
+            this.piece6.Size = new System.Drawing.Size(50, 60);
+            this.piece6.TabIndex = 6;
+            // 
+            // piece7
+            // 
+            this.piece7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.piece7.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.piece7.Location = new System.Drawing.Point(103, 198);
+            this.piece7.Name = "piece7";
+            this.piece7.Size = new System.Drawing.Size(50, 60);
+            this.piece7.TabIndex = 7;
+            // 
+            // piece8
+            // 
+            this.piece8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.piece8.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.piece8.Location = new System.Drawing.Point(159, 198);
+            this.piece8.Name = "piece8";
+            this.piece8.Size = new System.Drawing.Size(50, 60);
+            this.piece8.TabIndex = 8;
+            // 
+            // piece9
+            // 
+            this.piece9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.piece9.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.piece9.Location = new System.Drawing.Point(215, 198);
+            this.piece9.Name = "piece9";
+            this.piece9.Size = new System.Drawing.Size(50, 60);
+            this.piece9.TabIndex = 9;
+            // 
+            // conditionTextBox
+            // 
+            this.conditionTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.conditionTextBox.Location = new System.Drawing.Point(103, 262);
+            this.conditionTextBox.Name = "conditionTextBox";
+            this.conditionTextBox.Size = new System.Drawing.Size(162, 20);
+            this.conditionTextBox.TabIndex = 10;
+            this.conditionTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(190, 297);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 360);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.conditionTextBox);
+            this.Controls.Add(this.piece9);
+            this.Controls.Add(this.piece8);
+            this.Controls.Add(this.piece7);
+            this.Controls.Add(this.piece6);
+            this.Controls.Add(this.piece5);
+            this.Controls.Add(this.piece4);
+            this.Controls.Add(this.piece3);
+            this.Controls.Add(this.piece2);
+            this.Controls.Add(this.piece1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Tic-Tac-Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label piece1;
+        private System.Windows.Forms.Label piece2;
+        private System.Windows.Forms.Label piece3;
+        private System.Windows.Forms.Label piece4;
+        private System.Windows.Forms.Label piece5;
+        private System.Windows.Forms.Label piece6;
+        private System.Windows.Forms.Label piece7;
+        private System.Windows.Forms.Label piece8;
+        private System.Windows.Forms.Label piece9;
+        private System.Windows.Forms.TextBox conditionTextBox;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Programming_Project_4_Cameron_Deao/Programming_Project_4_Cameron_Deao/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Programming_Project_4_Cameron_Deao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Determing the 2D array values.
+        const int rows = 3;
+        const int cols = 3;
+        //Initializing the 2D array.
+        int[,] board = new int[rows, cols];
+        //Array to store the labels to be updated.
+        Label[] labels = new Label[9];
+        //Variables that will be used throughout the program.
+        int index = 0;
+        int EMPTY = 2;
+        int rowChoice = 0;
+        int colChoice = 0;
+        int winner = 2;
+        //Creating the random.
+        Random boardRoll = new Random();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Adding all the labels into an array.
+            labels[0] = piece1;
+            labels[1] = piece2;
+            labels[2] = piece3;
+            labels[3] = piece4;
+            labels[4] = piece5;
+            labels[5] = piece6;
+            labels[6] = piece7;
+            labels[7] = piece8;
+            labels[8] = piece9;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Setting the index to zero for each game.
+            index = 0;
+            //Filling the array with dummy values.
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int x = 0; x < board.GetLength(1); x++)
+                {
+                    board[i, x] = EMPTY;
+                }
+            }
+            //Determining which player will go first.
+            int firstPlayer = boardRoll.Next(0, 2);
+            //Bool to check if a winner has been found.
+            bool foundWinner = false;
+            //For loop used to populate the array.
+            for (int i = firstPlayer; i < (board.GetLength(0) * board.GetLength(1)) + firstPlayer; i++)
+            {
+                //Determining which index position will be chosen.
+                rowChoice = boardRoll.Next(0, 3);
+                colChoice = boardRoll.Next(0, 3);
+                //While loop to check if a dummy value is in the index.
+                while (board[rowChoice, colChoice] != EMPTY)
+                {
+                    rowChoice = boardRoll.Next(0, 3);
+                    colChoice = boardRoll.Next(0, 3);
+                }
+                //Setting the input with the use of a modulo.
+                board[rowChoice, colChoice] = i % 2;
+                //Checking if their is a winner.
+                foundWinner = CheckAndSetWinner();
+                //If winner is found the for loop breaks.
+                if (foundWinner)
+                {
+                    break;
+                }
+            }
+            //Outputting each value into the console.
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int x = 0; x < board.GetLength(1); x++)
+                {
+                    Console.WriteLine("INDEX VALUE: " + board[i,x].ToString());
+                }
+            }
+            //Updating the labels based on the 2D index values.
+            for(int i = 0; i < rows; i++)
+            {
+                for(int x = 0; x < cols; x++)
+                {
+                    if(board[i,x] == 0)
+                    {
+                        labels[index].Text = "O";
+                    }
+                    if (board[i, x] == 1)
+                    {
+                        labels[index].Text = "X";
+                    }
+                    else if (board[i, x] == 2)
+                    {
+                        labels[index].Text = "";
+                    }
+                    index += 1;
+                }
+            }
+            //Updating the textbox based on the winner.
+            if (foundWinner)
+            {
+                conditionTextBox.Text = "Winner is Team ";
+                conditionTextBox.Text += winner == 0 ? "X!" : "O!";
+            }
+            else
+            {
+                conditionTextBox.Text = "Tie Game!";
+            }
+        }
+        
+        //Logic used to check win conditions.
+        private bool CheckAndSetWinner()
+        {
+            bool foundWinner = false;
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[i, 0] == 0 && board[i, 1] == 0 && board[i, 2] == 0)
+                {
+                    winner = 1;
+                    foundWinner = true;
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[0, i] == 0 && board[1, i] == 0 && board[2, i] == 0)
+                {
+                    winner = 1;
+                    foundWinner = true;
+                }
+            }
+            if (board[0, 0] == 0 && board[1, 1] == 0 && board[2, 2] == 0)
+            {
+                winner = 1;
+                foundWinner = true;
+            }
+            if (board[0, 2] == 0 && board[1, 1] == 0 && board[2, 0] == 0)
+            {
+                winner = 1;
+                foundWinner = true;
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[i, 0] == 1 && board[i, 1] == 1 && board[i, 2] == 1)
+                {
+                    winner = 0;
+                    foundWinner = true;
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[0, i] == 1 && board[1, i] == 1 && board[2, i] == 1)
+                {
+                    winner = 0;
+                    foundWinner = true;
+                }
+            }
+            if (board[0, 0] == 1 && board[1, 1] == 1 && board[2, 2] == 1)
+            {
+                winner = 0;
+                foundWinner = true;
+            }
+            if (board[0, 2] == 1 && board[1, 1] == 1 && board[2, 0] == 1)
+            {
+                winner = 0;
+                foundWinner = true;
+            }
+            //Returns a bool value if winner is found.
+            return foundWinner;
+        }
+        //Close the application.
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
